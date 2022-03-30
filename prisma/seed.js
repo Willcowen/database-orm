b2619881334ac1ddac1a0f0ef476a4b3d4772211
willyc <willcowendrums@gmail.com>
2022-03-30 17:16:43 +0100

    part 5 complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,27 +1,46 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
-        }
-    });
+  const date = new Date(Date.parse("2022-10-01"));
+  const createCustomerWithContact = await prisma.customer.create({
+    data: {
+      name: "Alice",
+      contact: {
+        create: {
+          phone: "123",
+          email: "Alice@prisma.io",
+        },
+      },
+    },
+  });
 
-    console.log('Customer created', createdCustomer);
+  const createScreenWithScreening = await prisma.screen.create({
+    data: {
+      number: 123,
+      screening: {
+        create: [
+          {
+            startsAt: date,
+            movie: {
+              create: {
+                title: "TEST",
+                runtimeMins: 123,
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
 
-    // Add your code here
-
-
-
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  console.log("Customer with Contact created:", createCustomerWithContact);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
